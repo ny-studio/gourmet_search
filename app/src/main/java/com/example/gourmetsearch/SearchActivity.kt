fba20e0a74bb040822c7fece8078a098dcef19af
ny-studio <nysappstudio@gmail.com>
2022-05-21 21:18:44 +0900

    店舗ロゴ画像をダウンロードし表示する処理を追加

--- app/src/main/java/com/example/gourmetsearch/SearchActivity.kt
+++ app/src/main/java/com/example/gourmetsearch/SearchActivity.kt
@@ -2,23 +2,30 @@ package com.example.gourmetsearch
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.loader.content.AsyncTaskLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import okhttp3.ResponseBody
 import org.w3c.dom.NodeList
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.io.ByteArrayInputStream
+import java.io.StringReader
+import java.net.URL
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
@@ -28,9 +35,12 @@ class SearchActivity : AppCompatActivity(), LocationListener {
     val service = RetrofitClient.getService()
 
     //検索条件の変数
-    var latitude:Double = 0.0 // 緯度
-    var longitude:Double = 0.0 // 軽度
+    var latitude: Double = 0.0 // 緯度
+    var longitude: Double = 0.0 // 経度
     var radius = 5 // 検索範囲　1: 300m　2: 500m　3: 1000m (初期値)　4: 2000m　5: 3000m
+
+    //リスト表示するデータ
+    var restaurantsData: ArrayList<RestaurantData> = arrayListOf<RestaurantData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +48,7 @@ class SearchActivity : AppCompatActivity(), LocationListener {
 
         //検索ビュー
         val searchView = findViewById<SearchView>(R.id.search_view)
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 requestRestaurantData(query)
@@ -45,9 +56,14 @@ class SearchActivity : AppCompatActivity(), LocationListener {
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
+                if (!restaurantsData.isEmpty()) {
+                    restaurantsData.clear()
+                }
                 return false
             }
         })
+
+
     }
 
     override fun onResume() {
@@ -59,13 +75,14 @@ class SearchActivity : AppCompatActivity(), LocationListener {
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
 
-                val locationManager: LocationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+                val locationManager: LocationManager =
+                    getSystemService(LOCATION_SERVICE) as LocationManager
                 //GPSを起動
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, this)
 
                 //最後に確認された位置情報を取得
                 val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                if(location != null){
+                if (location != null) {
                     latitude = location.latitude    //緯度
                     longitude = location.longitude  //経度
                 }
@@ -75,7 +92,7 @@ class SearchActivity : AppCompatActivity(), LocationListener {
                 Toast.makeText(applicationContext, "位置情報が許可されていません。", Toast.LENGTH_SHORT).show()
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     //権限許可をリクエストする
-                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),0)
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
                 }
             }
         }
@@ -94,38 +111,57 @@ class SearchActivity : AppCompatActivity(), LocationListener {
                     call: Call<ResponseBody>,
                     response: Response<ResponseBody>
                 ) {
-                    var xml = ""
                     response.body()?.let {
-                        xml = it.string()
-                        println(response)
+                        parseXML(it.string())
                     }
-                    //xmlを解析してDOM Documentに変換するビルダー
-                    val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-                    //xmlを解析してDOM Documentを生成
-                    val stream = ByteArrayInputStream(xml.toByteArray())
-                    val document = builder.parse(stream)
-                    stream.close()
-                    //XPathを解釈するオブジェクトを生成
-                    val xPath = XPathFactory.newInstance().newXPath()
-
-                    //shopノードをすべて取得
-                    val shops = xPath.evaluate("//shop",document, XPathConstants.NODESET) as NodeList
-                    for(i in 0 until shops.length) {
-                        val shop = shops.item(i)
-                        val name = xPath.evaluate("./name/text()",shop)
-                        val logo = xPath.evaluate("./logo_image/text()",shop)
-                        val address = xPath.evaluate("./address/text()",shop)
-                        val genre = xPath.evaluate("./genre/name/text()",shop)
-                        print(name)
-                        println(genre)
-                        println(logo)
-                        println(address)
-                    }
+                    val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+                    recyclerView.adapter = MyRecyclerViewAdapter(restaurantsData)
+                    recyclerView.layoutManager = LinearLayoutManager(applicationContext)
                 }
 
                 override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                     Toast.makeText(applicationContext, "お店が見つかりませんでした。", Toast.LENGTH_SHORT).show()
                 }
             })
+    }
+
+    fun parseXML(xml: String) {
+        //xmlを解析してDOM Documentに変換するビルダー
+        val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        //xmlを解析してDOM Documentを生成
+        val stream = ByteArrayInputStream(xml.toByteArray())
+        val document = builder.parse(stream)
+        stream.close()
+        //XPathを解釈するオブジェクトを生成
+        val xPath = XPathFactory.newInstance().newXPath()
+
+        //shopノードをすべて取得
+        val shops = xPath.evaluate("//shop", document, XPathConstants.NODESET) as NodeList
+        for (i in 0 until shops.length) {
+            val shop = shops.item(i)
+            val name = xPath.evaluate("./name/text()", shop)
+            val logo = xPath.evaluate("./logo_image/text()", shop)
+            val address = xPath.evaluate("./address/text()", shop)
+            val genre = xPath.evaluate("./genre/name/text()", shop)
+
+            var restaurantData = RestaurantData(shop, "0", name, address,null , genre)
+
+            downloadImage(restaurantData).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,logo)
+        }
+    }
+
+    private inner class downloadImage(var restaurantData: RestaurantData) : AsyncTask<String,Int,Bitmap>() {
+
+        override fun doInBackground(vararg params: String?): Bitmap {
+            val url = URL(params[0])
+            val input = url.openStream()
+            val image = BitmapFactory.decodeStream(input)
+            return image
+        }
+
+        override fun onPostExecute(bitmap: Bitmap?){
+            restaurantData.logoImage = bitmap
+            restaurantsData.add(restaurantData)
+        }
     }
 }
