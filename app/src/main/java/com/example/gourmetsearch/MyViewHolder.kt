fc9b92bd96229e218c65631d549d93548662d0ab
ny-studio <nysappstudio@gmail.com>
2022-05-22 18:47:56 +0900

    住所、ジャンルを追加

--- app/src/main/java/com/example/gourmetsearch/MyViewHolder.kt
+++ app/src/main/java/com/example/gourmetsearch/MyViewHolder.kt
@@ -2,10 +2,14 @@ package com.example.gourmetsearch
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
     val logoImage:ImageView = itemView.findViewById(R.id.item_image)
     val nameText:TextView = itemView.findViewById(R.id.item_title)
+    val progressBar:ProgressBar = itemView.findViewById(R.id.loadimage_progress)
+    val addressText:TextView = itemView.findViewById(R.id.item_address)
+    val genreText:TextView = itemView.findViewById(R.id.item_genre)
 }
